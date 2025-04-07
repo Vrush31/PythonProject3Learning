5eee9c5eee16fbc18abecdcf6d94598daf7af6da
Vrush-git <vrushh31@gmail.com>
2025-04-07 23:31:58 +0530

    Apil7 all

---
+++ src/ex_070425/Lab06.py
@@ -0,0 +1,13 @@
+# tke user input a,b and calculate the sum,mult,substraction
+# we need  convert tring to intege by usung function called as int
+num1 = int(input("enter the num 1"))
+num2 = int(input("enter the num 2"))
+print("Sum is ", num1+num2)
+print("Sub is ", num1-num2)
+print("Mul is ", num1*num2)
+
+
+
+
+
+
---
+++ src/ex_070425/lab07.py
@@ -0,0 +1,14 @@
+#formating
+#format of string
+num= 90
+print("This is the number you working with "f" {num}")
+# it is replacing the value
+
+table= 9
+print(f"{table}*1={table}")
+print(f"{table}*2={table*2}")
+print(f"{table}*3={table*3}")
+
+#task create programe, take  2 inputs num1 and num2  1)make the max num and 2) give the power1 to power 2
+
+
